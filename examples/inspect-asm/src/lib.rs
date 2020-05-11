43e2e1d16e99897552a18856fa2942bdf05cc6db
yoanlcq <yoanlecoq.io@gmail.com>
2020-05-11 20:08:03 +0200

    Fix examples/inspect-asm since latest changes

--- examples/inspect-asm/src/lib.rs
+++ examples/inspect-asm/src/lib.rs
@@ -12,3 +12,7 @@ use vek::vec::*;
 #[no_mangle] pub fn v4_i32_zero() -> Vec4<i32> { Vec4::zero() }
 #[no_mangle] pub fn v4_i32_one () -> Vec4<i32> { Vec4::one()  }
 
+#[no_mangle] pub extern "C" fn v2_f32_dot_reprc (a: Vec2<f32>, b: Vec2<f32>) -> f32 { a.dot(b)  }
+#[no_mangle] pub extern "C" fn v2_f32_dot_simd (a: repr_simd::Vec2<f32>, b: repr_simd::Vec2<f32>) -> f32 { a.dot(b)  }
+#[no_mangle] pub extern "C" fn v2_f32_dot_reprc_handmade (a: Vec2<f32>, b: Vec2<f32>) -> f32 { a.x * b.x + a.y * b.y  }
+
