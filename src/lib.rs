10ad09d7878af075a60e9358353e9bc0b23f0866
Yoan Lecoq <yoanlecoq.io@gmail.com>
2022-11-27 22:32:58 +0100

    Bump version to 0.15.9; Mention #86

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@
 #![no_std]
 #![doc(
     test(attr(deny(warnings))),
-    html_root_url = "https://docs.rs/vek/0.15.8",
+    html_root_url = "https://docs.rs/vek/0.15.9",
     //html_logo_url = "https://yoanlcq.github.io/vek/logo.png",
     //html_favicon_url = "https://yoanlcq.github.io/vek/favicon.ico",
 )]
